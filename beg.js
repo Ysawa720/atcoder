52b98d9f0694491914028de7433e6f31559bfd94
Yuto Sawada <0720sawada@gmail.com>
2026-01-12 20:57:43 +0900

    ２問目追加

--- beg.js
+++ beg.js
@@ -1,13 +1,30 @@
-function Main(input) {
-    input = input.split("\n");
-    tmp = input[1].split(" ");
-    let a = parseInt(input[0], 10);
-    let b = parseInt(tmp[0], 10);
-    let c = parseInt(tmp[1], 10);
-    let d = input[2];
+
+//No.1
+// function Main(input) {
+//     input = input.split("\n");
+//     tmp = input[1].split(" ");
+//     let a = parseInt(input[0], 10);
+//     let b = parseInt(tmp[0], 10);
+//     let c = parseInt(tmp[1], 10);
+//     let d = input[2];
     
-    console.log(a+b+c, d)
+//     console.log(a+b+c, d)
+// }
+
+
+//No.2
+function Main(input) {
+    const temp = input.trim().split(" ");
+    const a = parseInt(temp[0], 10);
+    const b = parseInt(temp[1], 10);
+    const product = a * b; 
+    if (product % 2 === 0) {
+        console.log("Even");
+      } else {
+        console.log("Odd");
+      }
 }
-
-
+    
 Main(require("fs").readFileSync(0, "utf8"));
+
+
